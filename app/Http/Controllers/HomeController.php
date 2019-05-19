0a4e2a7f89cbd9a3549c05ff6a4f1f2255754c2f
anderdavid <root@localhost.localdomain>
2019-05-19 04:32:20 -0500

    Se crean rutas para test de autenticacion con roles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,22 @@ class HomeController extends Controller
     public function index(Request $request){
 
         $request->user()->authorizeRoles(['user', 'admin']);
+        //$request->user()->authorizeRoles(['usuario', 'superusuario']);
         return view('home');
     }
+    public function createUsers(Request $request){
+        $request->user()->authorizeRoles('admin');
+        echo "createUsers accesible solo por admin";
+    }
+    public function createOficinas(Request $request){
+         $request->user()->authorizeRoles('user');
+         echo "CreateOficinas accesible solo por users";
+    }
 
-    
+    public function ambos(Request $request){
+        $request->user()->authorizeRoles(['user', 'admin']);
+        echo "<h1>funcion accessible por todos los roles</h1>";
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/usuarios','HomeController@createUsers')->name('users');
+Route::get('/oficinas','HomeController@createOficinas')->name('oficinas');
+Route::get('/ambos','HomeController@ambos')->name('ambos');
