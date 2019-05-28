7b78896af128e126388d67596653bcfb04fb9488
anderdavid <root@localhost.localdomain>
2019-05-28 18:32:55 -0500

    Se crea controlador oficinasController con sus respectivas vistas

---
+++ resources/views/oficinas/edit.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>edit view</h1>
+	<p>id: {{$id}}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,23 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/create/puntosDeAtencion','HomeController@createPuntosDeAtencion')->name('puntosDeAtencion');
 Route::get('/create/oficinistas','HomeController@createOficinistas')->name('oficinistas');
 Route::get('/ambos','HomeController@ambos')->name('ambos');
+
+
+
+Route::get('/oficinas', 'OficinasController@index');
+Route::get('/oficinas/show', 'OficinasController@index');
+
+Route::get('/oficinas/create', 'OficinasController@create');
+Route::get('/oficinas/store', 'OficinasController@store');
+
+Route::get('/oficinas/show/{id}', 'OficinasController@show');
+Route::get('/oficinas/edit/{id}', 'OficinasController@edit');
+Route::get('/oficinas/update/{id}', 'OficinasController@update');
+
+Route::get('/oficinas/destroy/{id}', 'OficinasController@destroy');
+
+
+
+
+
+	
