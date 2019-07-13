6363975658f8c7c91cd99bc81724148fa8fb351b
anderdavid <root@localhost.localdomain>
2019-07-13 16:22:58 -0500

    Se estiliza scroll y grid

--- resources/views/puestos/asignarPuestos.blade.php
+++ resources/views/puestos/asignarPuestos.blade.php
@@ -1,16 +1,141 @@
 @extends('layouts.nav-administrador')
 @section('content')
- <!--  <div class="container"> -->
-    <h1>Asignar puestos</h1>
+<div class="container">
+  <h1>Asignar puestos</h1>
+  <div class="oficinistas-container">
+    <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+    <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
 
-    <div class="center-container">
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+     <div class="oficinista-container">
+      <div class="oficinista-img"></div>
+      <h3 id="oficicinsta-title">Alice</h3>
+    </div>
+
+  </div>
+
+
+  <div class="puestos-container">
+    <div class="puesto-container">
       <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-container">
-        <div class="oficinista-container">
-          <div class="oficinista-img"></div>
-          <h3 id="oficicinsta-title">Alice</h3>
-        </div>
-      </div>
-    </div><!-- 
-  --> </div>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+     <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+     <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+     <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+     <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+     <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+    <div class="puesto-container">
+      <h3 id="puesto-title">Puesto1</h3>
+      <div class="puesto-img"></div>
+    </div>
+  </div>
+
+</div>
+
 @endsection
