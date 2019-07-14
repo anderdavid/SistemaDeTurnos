b2c2865b89e880ce2744bc2d04ea301f04e2cbf6
anderdavid <root@localhost.localdomain>
2019-07-14 00:15:39 -0500

    Se estiliza drag and drop

--- resources/views/puestos/asignarPuestos.blade.php
+++ resources/views/puestos/asignarPuestos.blade.php
@@ -1,141 +1,71 @@
 @extends('layouts.nav-administrador')
 @section('content')
-<div class="container">
-  <h1>Asignar puestos</h1>
+<script>
+    function allowDrop(ev) {
+      ev.preventDefault();
+    }
+
+    function drag(ev) {
+      ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    }
+  </script>
+
+  <div class="container">
+    <h1 class="text-secondary">Asignar puestos</h1>
   <div class="oficinistas-container">
-    <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-    <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
+      <div id="drag1" class="card drag-container" draggable="true" ondragstart="drag(event)" >
+          <div class="oficinista-img"></div>
+          <h3 id="oficicinsta-title">Alice</h3> 
+      </div>
+       <div id="drag1" class="card drag-container" draggable="true" ondragstart="drag(event)" >
+          <div class="oficinista-img"></div>
+          <h3 id="oficicinsta-title">Alice</h3> 
+      </div>
+       <div id="drag1" class="card drag-container" draggable="true" ondragstart="drag(event)" >
+          <div class="oficinista-img"></div>
+          <h3 id="oficicinsta-title">Alice</h3> 
+      </div>
+       <div id="drag1" class="card drag-container" draggable="true" ondragstart="drag(event)" >
+          <div class="oficinista-img"></div>
+          <h3 id="oficicinsta-title">Alice</h3> 
+      </div>
+       <div id="drag1" class="card drag-container" draggable="true" ondragstart="drag(event)" >
+          <div class="oficinista-img"></div>
+          <h3 id="oficicinsta-title">Alice</h3> 
+      </div>
     </div>
 
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
 
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
-    </div>
-
-     <div class="oficinista-container">
-      <div class="oficinista-img"></div>
-      <h3 id="oficicinsta-title">Alice</h3>
+    <div class="puestos-container">
+      <div id="div1" class="card drop-container" >
+         <h3 id="puesto-title">Puesto1</h3>
+         <div class="puesto-img" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      </div>
+       <div id="div2" class="card drop-container" >
+         <h3 id="puesto-title">Puesto2</h3>
+         <div class="puesto-img" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      </div>
+       <div id="div3" class="card drop-container" >
+         <h3 id="puesto-title">Puesto3</h3>
+         <div class="puesto-img" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      </div>
+       <div id="div4" class="card drop-container" >
+         <h3 id="puesto-title">Puesto4</h3>
+         <div class="puesto-img" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      </div>
+       <div id="div5" class="card drop-container" >
+         <h3 id="puesto-title">Puesto5</h3>
+         <div class="puesto-img" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      </div>
+    
     </div>
 
   </div>
-
-
-  <div class="puestos-container">
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-     <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-     <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-     <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-     <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-     <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-    <div class="puesto-container">
-      <h3 id="puesto-title">Puesto1</h3>
-      <div class="puesto-img"></div>
-    </div>
-  </div>
-
-</div>
 
 @endsection
