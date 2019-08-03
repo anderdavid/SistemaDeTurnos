037e0dc53f41b3ef1507084a17d0ae02200d24c0
anderdavid <root@localhost.localdomain>
2019-08-03 15:35:13 -0500

    bug: Se quita checkbox recordar contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -19,7 +20,7 @@
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
-                                      <!--   <strong>{{ $message }}</strong> -->
+                                   
                                       <strong>Credenciales incorrectos. Revise su email y contraseña </strong>
                                     </span>
                                 @enderror
@@ -40,17 +41,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                       <!--  <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') !==null ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                        Recordar
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
